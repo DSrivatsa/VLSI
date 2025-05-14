f21e53ac6614523fe56daea0d8809a0ccee4a9c8
DSrivatsa <diddigisrivatsa2006@gmail.com>
2025-05-14 20:54:56 +0530

    Update tff_tb.v

--- T Flip Flop/tff_tb.v
+++ T Flip Flop/tff_tb.v
@@ -1,35 +1,30 @@
-module TFF_tb;
+//t flipflop testbench
+
+
+module T_flipflop_tb;
 reg T,clk,reset;
-wire q;
-T_ff dut(.T(T),.clk(clk),.reset(reset),.q(q));
+wire Q;
 
+T_ff dut (.T(T),.clk(clk),.reset(reset),.Q(Q));
+
+initial begin 
+	clk=0;
+	forever #5 clk=~clk;
+end
 
 initial begin
-clk=0;
-forever #5 clk=~clk;
+
+$display(" clk reset T | Q ~Q");
+$display("-------------------");
+
+reset=0;T=1;#10; 
+reset=0;T=0;#10; 
+reset=1;T=1;#10; 
+reset=1;T=0;#10; 
+
 end
-/*initial begin
-reset=0;T=0; #10;$Tisplay("%b %b %b | %b %b",clk,reset,T, q);
-#10;
-reset=0;T=1;  #10;$Tisplay("%b %b %b | %b %b",clk,reset,T, q);
-#10;
-reset=1;T=0;  #10;$Tisplay("%b %b %b | %b %b",clk,reset,T, q,~q);
-#10;
-reset=1;T=1; #10; $Tisplay("%b %b %b | %b %b",clk,reset,T, q,~q);
-#10;
-enT 
-enTmoTule*/
-integer i;
-initial begin
-for ( i=0; i<4; i=i+1)
-	   begin
-	   {reset,T}=i;
-	   #10;	
-end
-end
-initial 
-$display("clk reset T q ");
 initial
-$monitor("%b  %b  %b  |  %b  ", clk, reset,T, q);
-endmodule
+$monitor("  %b %b  %b |  %b  %b",clk,reset,T,Q,~Q);
 
+
+endmodule
