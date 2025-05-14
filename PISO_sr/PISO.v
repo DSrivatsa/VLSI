a4921dc4fb03d3f3d16d189b96c1a4fa9d51d34a
DSrivatsa <diddigisrivatsa2006@gmail.com>
2025-05-14 21:02:19 +0530

    Create PISO.v

---
+++ PISO_sr/PISO.v
@@ -0,0 +1,22 @@
+//parallel input- serial output shif register 
+
+
+module PISO(input [3:0]d,input clk,load, output reg q);
+reg [3:0] a;
+initial q=0;
+
+always@(posedge clk)
+begin
+	 if(load) begin
+	 a=d; end
+	 
+	 else begin
+	 q = a[0];
+	    a = {1'b0,a[3:1]};
+	    
+	    end
+	    
+	end
+endmodule
+	    
+	 
