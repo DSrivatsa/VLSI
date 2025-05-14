e6b80e3fe92f59dd16cac74a2c04069ecf2ce12e
DSrivatsa <diddigisrivatsa2006@gmail.com>
2025-05-14 20:57:20 +0530

    Update DFF_Asynch.v

--- D Flip Flop/DFF_Asynch.v
+++ D Flip Flop/DFF_Asynch.v
@@ -1,9 +1,15 @@
-module DFFasynch(input D, clk, reset, output reg Q);
-always @(posedge clk or posedge reset) begin
-Q<=0;
-if(reset)
-Q<=0;
-else
-Q<=D;
-end
+//D flipflop using non-blocking assignment (asynchronous reset)
+
+
+module D_flipflop(input D,clk,reset, output reg Q);
+  initial Q=0;
+  always@(posedge clk or posedge reset)
+   begin
+   
+	if (reset)	
+	  Q<=0;
+	else 
+	  Q<=D;
+   end	
 endmodule
+
