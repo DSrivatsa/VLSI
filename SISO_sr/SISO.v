08e3f6b5de7e9b1c3a503dd6e08687a41dd96655
DSrivatsa <diddigisrivatsa2006@gmail.com>
2025-05-14 20:59:53 +0530

    Create SISO.v

---
+++ SISO_sr/SISO.v
@@ -0,0 +1,16 @@
+//serial ip-serial op  shift register (4 bits)
+
+
+module SISO(input d ,clk,output reg q);
+ reg [3:0] a;
+
+ initial begin q=0;a=0; end
+
+ always@(posedge clk)
+  begin 
+    a <= {a[2:0],d};
+    q=a[3];
+    end  
+  
+   
+  endmodule
